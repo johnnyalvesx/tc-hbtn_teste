743b73655bbf1101ef94b4a2e04dfd7a279593ae
johnnyalvesx <alvesjohnny@outlook.com>
2022-03-14 20:06:03 -0300

    Testes de software - Introdução - Task 0

---
+++ 0x00/email/Pessoa.java
@@ -0,0 +1,8 @@
+public class Pessoa {
+
+    private String email;
+
+    public static boolean emailValid(String email) {
+	return (email.length() <= 50 || email.contains("@")) ? true : false;
+    }
+}
